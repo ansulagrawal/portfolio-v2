4887b5aed31b7cd8491d5b3de649b2cd558c3411
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-05-13 01:50:40 +0530

    Added Navbar

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <p>Hello world!</p>;
 }
 
 export default Page;
---
+++ config/global.js
@@ -0,0 +1,35 @@
+export const siteConfig = {
+  name: 'Ansul Agrawal',
+  title: 'Ansul Agrawal - Portfolio',
+  description: 'Ansul Agrawal - Portfolio built using Next.js, Tailwind CSS',
+  keywords: ['Ansul Agrawal, Portfolio, Next.js, Tailwind CSS'],
+  url: 'https://ansulagrawal.in',
+  author: [
+    {
+      name: 'Ansul Agrawal',
+      url: 'https://ansulagrawal.in',
+    },
+  ],
+  links: {
+    github: 'https://github.com/ansulagrawal',
+  },
+  creator: 'Ansul Agrawal',
+};
+
+export const routes = [
+  {
+    key: 'home',
+    title: 'Home',
+    path: '/',
+  },
+  {
+    key: 'about',
+    title: 'About',
+    path: '/about',
+  },
+  {
+    key: 'project',
+    title: 'Projects',
+    path: '/projects',
+  },
+];
