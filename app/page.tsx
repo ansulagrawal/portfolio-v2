30204560ae35c2dcf4282f10cd6914b009dc28a9
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-05-22 23:43:39 +0530

    Added FloatingNav and About Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Grid from '@/components/Home/Grid';
 import Hero from '@/components/Home/Hero';
-import React from 'react';
+import { FloatingNav } from '@/components/ui/FloatingNav';
+import { navItems } from '@/data';
 
 function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
